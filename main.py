121eae753537955e97f656b24a1fa037332468a7
JKPradhan95 <168675593+JKPradhan95@users.noreply.github.com>
2024-06-09 07:13:13 +0000

    Send Rich HTML Email  from Outlook or Hot Mail

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 sender = "senderdemo@gmail.com"
 receiver = "receiverdemo@gmail.com"
 password = "password123"
 
-message = """\
- Subject : Hello Hello 
- 
- This is jay
- Just wanted to say hi!"""
+message = MIMEMultipart()
+message['From'] = sender
+message['To'] = receiver
+message['Subject'] = "Hello Again!"
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender,password)
-server.sendmail(sender, receiver, message)
-server.quit()
+body = """
+<h2>Hi there!</h2>
+There are only a few things you need to do to get this email to you.
+"""
+mimetext = MIMEText(body, 'html')
+message.attach(mimetext)
 
+server = None
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    server.login(sender, password)
+
+    message_text = message.as_string()
+    server.sendmail(sender, receiver, message_text)
+    print("Email sent successfully")
+
+except smtplib.SMTPAuthenticationError:
+    print("Failed to login. Check your username and password.")
+except smtplib.SMTPConnectError:
+    print("Failed to connect to the SMTP server. Check the server address and port.")
+except smtplib.SMTPException as e:
+    print(f"An error occurred: {e}")
+finally:
+  if server is not None:
+    server.quit()
